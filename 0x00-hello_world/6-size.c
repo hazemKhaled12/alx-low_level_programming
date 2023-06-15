10b17f88649b8ee25c21ddab4389415115b42207
Hazem <hazem.khaled1293@gmail.com>
2023-06-15 16:14:04 -0700

    first c program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: A C program that prints size of types
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	printf("Size of a int: %lu byte(s)\n",sizeof(char));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(char));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(char));
+	printf("Size of a float: %lu byte(s)\n",sizeof(char));
+	return (0);
+}
