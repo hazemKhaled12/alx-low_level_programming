529549b1c91cb52c8f592b8f81fba636f2dbe066
Hazem <hazem.khaled1293@gmail.com>
2023-06-15 15:55:07 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ * Description: A C program that prints using put
+ * Return: Always 0
+*/i
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
