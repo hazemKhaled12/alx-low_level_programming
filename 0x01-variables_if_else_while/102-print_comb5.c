00aec22e964db6090c26ca887b3f9f47c6d1a36f
Hazem <hazem.khaled1293@gmail.com>
2023-06-16 10:38:24 -0700

    last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,23 +19,23 @@ int main(void)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
-				punchar((num1 / 10) + 48);
-				punchar((num1 % 10) + 48);
-				punchar((' '));
-				punchar((num2 / 10) + 48);
-				punchar((num2 % 10) + 48);
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar((' '));
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
 
 				if (num1 != 98 || num2 != 99)
 				{
-					punchar(',');
-					punchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 			num2++;
 		}
 		num1++;
 	}
-	punchar('\n');
+	putchar('\n');
 	return (0);
 
 }
