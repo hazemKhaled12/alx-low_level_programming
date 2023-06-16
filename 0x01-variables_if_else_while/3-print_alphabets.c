6c87ba5c4d35fe37c0def9ae01f4bc1dcf821403
Hazem <hazem.khaled1293@gmail.com>
2023-06-16 03:09:39 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphapet
+ *
+ * Description: this is a function to print the alphapet
+ *
+ * Return: the function always return (0)
+*/
+int main(void)
+{
+	char ch = a;
+
+	for (ch; ch <= z; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
