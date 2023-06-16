29b5f0e1edcc40461885cd4d3b465af0b2d29ee9
Hazem <hazem.khaled1293@gmail.com>
2023-06-16 09:33:08 -0700

    treble number

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,11 +25,12 @@ int main(void)
 					putchar(num1 + '0');
 					putchar(num2 + '0');
 					putchar(num3 + '0');
-				}
-				if (num1 + num2 + num3 != 24)
-				{
-					putchar(',');
-					putchar(' ');
+				
+					if (num1 + num2 + num3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 				num3++;
 			}
