8fa3d11c406a49dfd58e9f01b8364ebc865dc63f
Hazem <hazem.khaled1293@gmail.com>
2023-06-16 07:56:35 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
