ebd644f93a7ff4b84679ae198cafd66bc4bc48cd
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 15:31:14 -0700

    print last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_last_digit- entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * @n: input for the function
+ *
+ * Return: always 0
+*/
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
