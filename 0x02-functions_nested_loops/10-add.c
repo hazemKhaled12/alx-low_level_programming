4403b6df4b2dd1eb6402d947b604b5956a890f5a
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 16:49:57 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * add - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * @a: input for the function
+ * @b: another input
+ *
+ * Return: always 0
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	return (a + b);
+}
