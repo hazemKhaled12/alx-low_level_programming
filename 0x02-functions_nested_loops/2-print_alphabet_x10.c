90acfd485e21be5b36f08c38496ce5657b6829a6
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 14:39:49 -0700

    print aphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - the function
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int counter = 0;
+
+	while (counter < 11)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
