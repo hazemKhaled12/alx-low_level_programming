a206d96f37ffc46b5fc9e93601f76f803cbda2b3
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 13:54:13 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: function prints _putchar
+ *
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+02 nested functions readme file
