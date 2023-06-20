b28c130037d5468b9d7ce2b4721e582318ff306f
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 15:27:03 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _abs - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * @n: input for the function
+ *
+ * Return: always 0
+*/
+
+int _abs(int n)
+{
+	int final;
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return n
+}
