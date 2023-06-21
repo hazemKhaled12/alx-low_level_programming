a62a23d222a5bd1b2fb3b4ac93ffac388cb8c977
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 17:20:37 -0700

    dynamic times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 			if (prod >= 100)
 			{
 				_putchar((prod / 100) + '0');
-				_putchar((prod / 10) + '0');
+				_putchar(((prod / 10) % 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
 			if (num == n)
