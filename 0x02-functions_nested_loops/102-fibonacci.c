750a5b0221c72d312d85b29f897b1a99a5b79b15
Hazem <hazem.khaled1293@gmail.com>
2023-06-20 17:39:45 -0700

    50 fib numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ **
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i;
+	int fib[50] = {1, 2};
+
+	printf("%d, %d", fin[0], fin[1]);
+
+	i = 2;
+	while (i < 50)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		printf(" ,%d", fib[i]);
+	}
+	printf("\n");
+
+}
