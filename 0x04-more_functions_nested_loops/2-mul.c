f5ff1416e53897d7ea69b2d4e6c6eefde8327a4f
hazemKhaled12 <hazem.khaled1293@gmail.com>
2023-06-22 23:26:45 +0000

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * mul - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * @a: input for the function
+ *
+ * @b: another number
+ *
+ * Return: always the multiply result of a and b
+*/
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
