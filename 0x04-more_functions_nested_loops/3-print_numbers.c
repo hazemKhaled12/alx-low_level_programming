af8c3161667c78d8093c41a683067fcaf413df78
hazemKhaled12 <hazem.khaled1293@gmail.com>
2023-06-22 23:35:56 +0000

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+}
