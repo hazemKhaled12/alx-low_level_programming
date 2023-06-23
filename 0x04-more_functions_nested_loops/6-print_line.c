a558dbfe216c2b3a8436a8988b8058e08ae85294
hazemKhaled12 <hazem.khaled1293@gmail.com>
2023-06-23 00:39:33 +0000

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * @n: paramateter for the function
+ *
+ * Return: always 0
+*/
+
+void print_line(int n)
+{
+	int start = 0;
+
+	if (n > 0)
+	{
+		while (start < n)
+		{
+			_putchar('-');
+		}
+	}
+	_putchar('\n');
+}
