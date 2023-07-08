21e9927b094ca50db8820a0017b69e08d4f7d2c3
hazemKhaled12 <hazem.khaled1293@gmail.com>
2023-07-08 01:04:52 +0000

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++
+	}
+	return (0);
+}
