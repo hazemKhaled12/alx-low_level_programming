04b3e212a37e83666f0d5125ef5a5e0fc0fb3c28
hazemKhaled12 <hazem.khaled1293@gmail.com>
2023-08-01 21:32:18 +0000

    a

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -17,7 +17,7 @@ size_t free_listint_safe(listint_t **h)
 
 	while (*h)
 	{
-		diff = *h - (*h)->next;
+		diff = (char*)(*h) - (char*)((*h)->next);
 		if (diff > 0)
 		{
 			temp = (*h)->next;
@@ -34,7 +34,7 @@ size_t free_listint_safe(listint_t **h)
 		}
 	}
 
-	*h = NULL;
+	/*h = NULL;*/
 
 	return (len);
 }
