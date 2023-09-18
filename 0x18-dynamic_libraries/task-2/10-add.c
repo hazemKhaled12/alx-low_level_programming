ff1831f98ec3bf3726efce9b8a3b6cf68c5493ce
hazem Khaled <hazem.khaled1293@gmail.com>
2023-09-19 01:36:55 +0300

    task 3

---
+++ 0x18-dynamic_libraries/task-2/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * add - entry point
+ *
+ * Description: Print numbers from 0 to 9
+ *
+ * @a: input for the function
+ * @b: another input
+ *
+ * Return: always 0
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
